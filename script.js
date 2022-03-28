2e42899404bf3e3d5c412380fac5a0e2bf8b46ef
Fitz Hawke <will@fitzhawk.com>
2022-03-28 17:10:21 -0400

    Add ability to change number of cells in board

--- script.js
+++ script.js
@@ -5,27 +5,54 @@ function resetBoard() {
     });
 }
 
-const board = document.querySelector(".board");
-let gridSize = 16;
-let cellSize = 600 / gridSize;
-for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-        const div = document.createElement("div");
-        div.classList.add("cell");
-        div.setAttribute(
-            "style",
-            `width: ${cellSize}px; height: ${cellSize}px`
-        );
-        board.appendChild(div);
-    }
+function darkenCell() {
+    this.classList.add("dark-cell");
 }
 
-const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => {
-        cell.classList.add("dark-cell");
+function destroyGrid() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.removeEventListener("mouseover", darkenCell);
+        cell.remove();
     });
-});
+}
+
+function buildGrid(gridSize) {
+    const board = document.querySelector(".board");
+    let cellSize = 600 / gridSize;
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const div = document.createElement("div");
+            div.classList.add("cell");
+            div.setAttribute(
+                "style",
+                `width: ${cellSize}px; height: ${cellSize}px`
+            );
+            board.appendChild(div);
+        }
+    }
+
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover", darkenCell);
+    });
+}
+
+function getSize() {
+    return window.prompt(
+        "How many cells do you want per row of the grid?",
+        "16"
+    );
+}
+
+buildGrid(16);
 
 const reset = document.querySelector(".reset");
 reset.addEventListener("click", resetBoard);
+
+const size = document.querySelector(".size");
+size.addEventListener("click", () => {
+    newSize = getSize();
+    destroyGrid();
+    buildGrid(newSize);
+});
