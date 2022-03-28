0c8366d62e267d1383e44667d54ecf7002766983
Fitz Hawke <will@fitzhawk.com>
2022-03-28 15:30:59 -0400

    Small fixes for basic layout

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const board = document.querySelector('.board');
+const board = document.querySelector(".board");
 let gridSize = 16;
-let cellSize = window.getComputedStyle(board).width / gridSize;
-console.log(cellSize);
-for (let i = 0; i<gridSize; i++){
-    for (let j=0; j<gridSize; j++){
-        const div = document.createElement('div');
-        div.classList.add('cell');
-        div.setAttribute('style', `width: ${cellSize}px; height: ${cellSize}px`);
+let cellSize = 600 / gridSize;
+for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        div.setAttribute(
+            "style",
+            `width: ${cellSize}px; height: ${cellSize}px`
+        );
         board.appendChild(div);
     }
 }
